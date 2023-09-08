8d2998625ebd06c8f5c1cb8e6e98fde25268b05f
nahid <eng.yanahid@gmail.com>
2023-09-08 18:24:19 +0600

    Returning Objects From Controller

--- src/main/java/io/javabrains/springbootquickstrat/TopicController.java
+++ src/main/java/io/javabrains/springbootquickstrat/TopicController.java
@@ -3,11 +3,18 @@ package io.javabrains.springbootquickstrat;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class TopicController {
 
     @RequestMapping("/topics")
-    public String getAllTopics() {
-        return "All Topics";
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+                new Topic("spring","Spring Framework", "Spring Framework Description"),
+                new Topic("java","Core Java","Core Java Description"),
+                new Topic("javascript","Javascript","Javascript Description")
+        );
     }
 }
