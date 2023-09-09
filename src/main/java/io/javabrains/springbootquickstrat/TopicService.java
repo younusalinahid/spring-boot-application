13eeaf1b32acc844a5d6247429df6060577ff330
nahid <eng.yanahid@gmail.com>
2023-09-09 16:50:33 +0600

    Getting a single resource

--- src/main/java/io/javabrains/springbootquickstrat/TopicService.java
+++ src/main/java/io/javabrains/springbootquickstrat/TopicService.java
@@ -16,4 +16,7 @@ public class TopicService {
     public List<Topic> getAllTopics() {
         return topics;
     }
+    public Topic getTopic(String id) {
+        return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+    }
 }
