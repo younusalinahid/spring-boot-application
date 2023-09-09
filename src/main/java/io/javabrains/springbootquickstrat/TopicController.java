eeb57ee955f3fb2120e0658c41a04dcc166f84ac
nahid <eng.yanahid@gmail.com>
2023-09-09 15:57:52 +0600

    Topic Service

--- src/main/java/io/javabrains/springbootquickstrat/TopicController.java
+++ src/main/java/io/javabrains/springbootquickstrat/TopicController.java
@@ -1,5 +1,6 @@
 package io.javabrains.springbootquickstrat;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +10,10 @@ import java.util.List;
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
-        return Arrays.asList(
-                new Topic("spring","Spring Framework", "Spring Framework Description"),
-                new Topic("java","Core Java","Core Java Description"),
-                new Topic("javascript","Javascript","Javascript Description")
-        );
+        return topicService.getAllTopics();
     }
 }
